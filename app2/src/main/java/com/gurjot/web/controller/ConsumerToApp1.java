fb95eb83bafff5eacbd1181f12c956dd351f9247
Gurjot Singh <gurjot@fullstacklabs.ca>
2021-02-26 16:59:49 -0500

    props

--- app2/src/main/java/com/gurjot/web/controller/ConsumerToApp1.java
+++ app2/src/main/java/com/gurjot/web/controller/ConsumerToApp1.java
@@ -2,6 +2,7 @@ package com.gurjot.web.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,7 +11,7 @@ import org.springframework.web.client.RestTemplate;
 
 @RestController
 @RequestMapping("/app2")
-//@RefreshScope // for cloud config
+@RefreshScope // for cloud config
 //@EnableAutoConfiguration
 public class ConsumerToApp1 {
 
